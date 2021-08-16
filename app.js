d814979aecf8a62e34f3fa8500f14f722ebddc78
unknown <gartoyou@gmail.com>
2021-08-16 16:01:17 +1200

    Change port

--- app.js
+++ app.js
@@ -8,10 +8,10 @@ app.get ("/", function(req,res){
 // Static folder
 app.use(express.static(__dirname + "/public"));
 
-app.listen(3000, () => {
-  console.log('Persus Vincere listening on port 3000!');
-});
-
-// app.listen(process.env.PORT, process.env.IP, function(){
-//    console.log("Persus Vincere Server Has Started!");
+// app.listen(3000, () => {
+//   console.log('Persus Vincere listening on port 3000!');
 // });
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Persus Vincere Server Has Started!");
+});
